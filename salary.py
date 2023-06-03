d296d66b7df0624089007dab4e15068dafe2ba2e
Wojciech Przybyła <contact@wpww.pl>
2023-06-03 11:19:41 +0200

    pracownik oblicz_netto

--- salary.py
+++ salary.py
@@ -4,3 +4,16 @@ class Pracownik:
     self.last_name = last_name
     self.salary = salary
 
+  def oblicz_netto(self):
+    skladka_emeryt = self.salary * 0.0976
+    skladka_rentowa = self.salary * 0.015
+    skladka_ubezp_chor = self.salary * 0.0245
+    podstawa_na_zdrowotne = self.salary - skladka_emeryt - skladka_rentowa - skladka_ubezp_chor
+
+    skladka_zdrowotna = podstawa_na_zdrowotne * 0.09
+
+    podstawa_na_dochodowy = round(podstawa_na_zdrowotne - 250, 0)
+    zaliczka_na_dochodowy = 0.12 * podstawa_na_dochodowy - 300
+    
+    return round(podstawa_na_zdrowotne - skladka_zdrowotna - zaliczka_na_dochodowy, 2)
+
