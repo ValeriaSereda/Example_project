f5e9146f24c4db1b40d5a10fb4e46adde1f25f55
Wojciech Przybyła <contact@wpww.pl>
2023-06-03 10:16:00 +0200

    pyramid

--- main.py
+++ main.py
@@ -29,3 +29,21 @@ assert cube_params (3) == { "volume": 27, "area": 54 }
 assert cube_params (-1) == { "code error"}
 assert cube_params (0.5) == { "volume": 0.125, "area": 1.5}
 assert cube_params (10) == { "volume": 1000, "area": 600}
+
+def pyramid_params(base_edge: float, side_height: float, height: float):
+  """This function computes and returns volume and area of a four-sided regular pyramid with a given edges' lengths and height."""
+
+  if base_edge < 0 or side_height < 0 or height < 0:
+    return "error: one of the values is negative"
+  
+  # assuming four-sidedness
+  base_area = base_edge ** 2
+  side_area = base_edge * side_height / 2
+  total_area = base_area + 4 * side_area
+
+  volume = base_area * height / 3
+
+  return {
+    "volume": volume,
+    "area": total_area
+  }
