456146835296e9d74b0ec0290e4c6cc6115e5d60
ValeriaSereda <seredylka.lera@gmail.com>
2023-06-03 10:15:54 +0200

    zmiana_kuli_na_stozek

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 # funkcja liczaca objetosc kuli 
-def kula(r):
+def stozek(r,h):
     if r < 0:
      return ("error: radius is negative")
+    if h < 0:
+      return ("error: high is negative")
     pi = 3.14159265359
-    ob_kula = 4/3*pi*(r**3)
-    return ob_kula 
-print (f'Obijetosc kuli wynosi: {kula(2)}')
+    ob_stozek = 1/3*pi*(r**2)*h
+    return ob_stozek
+print (f'Obijetosc stożka wynosi: {stozek(3,2)}')
 
 assert kula(5) == 523.5987755983333
 assert kula(3.4) == 164.63621020893513
 assert kula(0) == 0
 assert kula(-3) == "error: radius is negative"
+
+def cube_params(side: float):
+  """This function computes and returns volume and area of a cube with a given side length."""
+
+  if side < 0:
+    return { "code error" }
+
+  volume = side ** 3
+  area = 6 * side ** 2
+  return {
+    "volume": volume,
+    "area": area 
+  }
+
+assert cube_params (3) == { "volume": 27, "area": 54 }
+assert cube_params (-1) == { "code error"}
+assert cube_params (0.5) == { "volume": 0.125, "area": 1.5}
+assert cube_params (10) == { "volume": 1000, "area": 600}
