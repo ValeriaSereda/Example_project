85d13a2bd580a852b21e309bdc386849e7aa75ce
ValeriaSereda <seredylka.lera@gmail.com>
2023-06-12 18:36:37 +0200

    correction

--- salary.py
+++ salary.py
@@ -5,7 +5,7 @@ class Pracownik:
         self.salary = salary
     
     def __str__(self):
-        return self.first_name + ',' + self.last_name + ',' + str(self.salary)
+        return self.first_name + ' ' + self.last_name + ',' + ' ' + str (self.salary)
     
     def koszty_pracodawcy(self) -> float:
         skladka_emerytalna = self.salary * 0.0976
@@ -15,12 +15,12 @@ class Pracownik:
         skladka_na_Fund_Gwar_Swiad_Prac = self.salary * 0.001
         laczne_skladki_na_ubezp_spol = skladka_emerytalna + skladka_na_Fund_Gwar_Swiad_Prac + skladka_na_Fundusz_pracy + skladka_na_ubezpieczenie_wypadkowe + skladka_rentowa
         koszty_pracodawcy = self.salary + laczne_skladki_na_ubezp_spol 
-        return (koszty_pracodawcy) 
+        return round (koszty_pracodawcy, 2) 
         
-Pracownik_1 = Pracownik('Jan', 'Kowalski',3500) 
+Pracownik_1 = Pracownik('Jan', 'Kowalski', 3500) 
 Pracownik_2 = Pracownik('Tomasz', "Torpeda", 3450.553)
 
-assert print(Pracownik_1) == "Jan Kowalski, 3500"
-assert print(Pracownik_2) == "Tomasz Torpeda, 3450.55"
-assert Pracownik_1.koszty_pracodawcy() == 4216.8
+assert str(Pracownik_1) == "Jan Kowalski, 3500"
+assert str(Pracownik_2) == "Tomasz Torpeda, 3450.553"
+assert Pracownik_1.koszty_pracodawcy() == 4216.80
 assert Pracownik_2.koszty_pracodawcy() == 4157.23
